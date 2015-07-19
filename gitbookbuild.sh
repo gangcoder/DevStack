ee42fb2b26bf313491a5a135ca5b63962ac3f4de
xugang <xugang@feeyo.com>
2015-07-19 22:51:06 +0800

    add shell variable and read input

--- gitbookbuild.sh
+++ gitbookbuild.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+read commit
+git add -A
+git commit -m "$commit"
 echo 'build gitbook start...'
 gitbook build
 echo 'copy static html to githuo.io...'
 cp -u -r _book/* /home/vagrant/git/hubeixugang.github.io
 echo 'copy done'
+cd /home/vagrant/git/hubeixugang.github.io
+git add -A
+git commit -m "$commit"
+git push origin master
+cd /home/vagrant/git/Accumulate
+echo 'push done'
