7ba97e57c3e4ec78c32b7acba8f9885e087898ce
xugang <xugang@feeyo.com>
2015-11-20 22:59:37 +0800

    增加 链栈

--- Foundation/DataStructure/stack.c
+++ Foundation/DataStructure/stack.c
@@ -87,3 +87,42 @@ Status Pop(SqDoubleStack *S, SElemType *e, int stackNumber)
     }
     return OK;
 }
+
+/*
+ * 链栈
+ */
+typedef struct StackNode
+{
+    SElemType data;
+    struct StackNode *next;
+}StackNode, *LinkStackPtr;
+
+typedef struct LinkStack
+{
+    LinkStackPtr top;
+    int count;
+}LinkStack;
+
+Status Push(LinkStack *S, SElemType e)
+{
+    LinkStackPtr s = (LinkStackPtr)malloc(sizeof(StackNode));
+    s->data = e;
+    s->next = S->top;
+    S->top = s;
+    S->count ++;
+    return OK;
+}
+
+Status Push(LinkStack *S, SElemType e)
+{
+    LinkStackPtr p;
+    if (StackEmpty(*S))
+        return ERROR;
+    *e = S->top->data;
+    p = S->top;
+    S->top = S->top->next;
+    
+    free(p)
+    S->count --;
+    return OK;
+}
